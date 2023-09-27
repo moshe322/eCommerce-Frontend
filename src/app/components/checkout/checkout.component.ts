41dd8743a3f07edc447f40321ec29e37d52de673
Pedro Costa <pedrocostaalves@live.com.pt>
2023-09-27 12:36:34 +0100

    Populated Countries and States
    Used country-state-city package

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CartService } from 'src/app/services/cart.service';
+import { FormService } from 'src/app/services/form.service';
+import { Country, State } from 'country-state-city';
+import { ICountry, IState } from 'country-state-city';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
+
   styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
   checkoutFormGroup: FormGroup = new FormGroup({});
-  isSubmitted = false;
 
+  private _formBuilder: FormBuilder;
   private _cartService: CartService;
+  private _formService: FormService;
 
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder, cartService: CartService) {
+  months: number[] = [];
+  years: number[] = [];
+
+  countries: ICountry[] = [];
+  statesShipping: IState[] = [];
+  statesBilling: IState[] = [];
+
+  constructor(formBuilder: FormBuilder, cartService: CartService) {
+    this._formBuilder = formBuilder;
     this._cartService = cartService;
+    this._formService = new FormService();
   }
 
   /**
@@ -26,28 +40,31 @@ export class CheckoutComponent implements OnInit {
    * @memberof CheckoutComponent
    */
   ngOnInit(): void {
+    this.loadCountries();
+    this.loadMonths(new Date().getMonth() + 1);
+    this.loadYears();
     this.setCartDetails();
-    this.checkoutFormGroup = this.formBuilder.group({
-      customer: this.formBuilder.group({
+    this.checkoutFormGroup = this._formBuilder.group({
+      customer: this._formBuilder.group({
         firstName: [''],
         lastName: [''],
         email: [''],
       }),
-      shippingAddress: this.formBuilder.group({
+      shippingAddress: this._formBuilder.group({
         street: [''],
         city: [''],
         state: [''],
         country: [''],
         zipCode: [''],
       }),
-      billingAddress: this.formBuilder.group({
+      billingAddress: this._formBuilder.group({
         street: [''],
         city: [''],
         state: [''],
         country: [''],
         zipCode: [''],
       }),
-      creditCard: this.formBuilder.group({
+      creditCard: this._formBuilder.group({
         cardType: [''],
         nameOnCard: [''],
         cardNumber: [''],
@@ -79,6 +96,45 @@ export class CheckoutComponent implements OnInit {
   }
 
   /**
+   * Change states, when country is changed.
+   * If number is 1, change shipping states.
+   * If number is 2, change billing states.
+   * @param number
+   * @memberof CheckoutComponent
+   */
+  changeStates(number: number) {
+    if (number == 1) {
+      const shippingFormGroup = this.checkoutFormGroup.get('shippingAddress');
+      const selectedCountry: string = shippingFormGroup?.value.country;
+      this.statesShipping = State.getStatesOfCountry(selectedCountry);
+    }
+    if (number == 2) {
+      const billingFormGroup = this.checkoutFormGroup.get('billingAddress');
+      const selectedCountry: string = billingFormGroup?.value.country;
+      this.statesBilling = State.getStatesOfCountry(selectedCountry);
+    }
+  }
+
+  /**
+   * Change months.
+   * If selected year is current year, start month is current month.
+   * Else start month is 1.
+   * @memberof CheckoutComponent
+   */
+  changeMonthsByYear() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+    const selectedYear: number = creditCardFormGroup?.value.expirationYear;
+    const currentYear: number = new Date().getFullYear();
+    let startMonth: number = 1;
+
+    if (selectedYear == currentYear) {
+      startMonth = new Date().getMonth() + 1;
+    }
+
+    this.loadMonths(startMonth);
+  }
+
+  /**
    * Set cart details.
    * Get total price and total quantity.
    * @private
@@ -92,6 +148,36 @@ export class CheckoutComponent implements OnInit {
     this._cartService
       .getTotalQuantity()
       .subscribe((totalQuantity) => (this.totalQuantity = totalQuantity));
+  }
+
+  /**
+   * Load countries.
+   * @private
+   * @memberof CheckoutComponent
+   */
+  private loadCountries() {
+    this.countries = Country.getAllCountries();
+  }
+
+  /**
+   * Load months.
+   * @param startMonth
+   * @memberof CheckoutComponent
+   */
+  private loadMonths(startMonth: number) {
+    this._formService.getCreditCardMonths(startMonth).subscribe((data) => {
+      this.months = data;
+    });
+  }
+
+  /**
+   * Load years.
+   * @memberof CheckoutComponent
+   */
+  private loadYears() {
+    this._formService.getCreditCardYears().subscribe((data) => {
+      this.years = data;
+    });
   }
 
   /**
